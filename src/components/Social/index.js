30f04e66bdc4132eb8eed1d14d794628f3c6e9bc
sapsari <sapsari@gmail.com>
2019-06-26 16:47:01 +0300

    header image

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -12,7 +12,7 @@ function Social({ website, twitter, github, linkedin, email, rssfeed }) {
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
-            viewBox="0 0 24 24"
+            viewBox="-7 -4 31 34"
             fill="none"
             stroke="currentColor"
             strokeWidth="2"
